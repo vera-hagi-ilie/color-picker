43b69d898de0494f4349503aa8897baae5db2570
Vera H <vera.hagi@yahoo.com>
2020-10-16 22:02:02 +0000

    Updates

---
+++ precache-manifest.7a0f117d8f6dc5c4cbfab6528e921473.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b91830f51513031cc1a0d914824aab",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "530c173bb8555e5f02f4",
+    "url": "/color-picker/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "6f00d6df588f6b526f96",
+    "url": "/color-picker/static/css/main.d6c914d5.chunk.css"
+  },
+  {
+    "revision": "530c173bb8555e5f02f4",
+    "url": "/color-picker/static/js/2.9efabd3f.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/color-picker/static/js/2.9efabd3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f00d6df588f6b526f96",
+    "url": "/color-picker/static/js/main.d7ed1127.chunk.js"
+  },
+  {
+    "revision": "5d8b67068727d6c44189",
+    "url": "/color-picker/static/js/runtime-main.a0d295b7.js"
+  },
+  {
+    "revision": "d81a8abe110357b7f8bd0b2d56eb98ea",
+    "url": "/color-picker/static/media/background.d81a8abe.svg"
+  }
+]);
